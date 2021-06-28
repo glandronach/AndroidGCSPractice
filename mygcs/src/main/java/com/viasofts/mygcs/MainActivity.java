1e78eb96981b4a2f9d74a494f6a5095904c533df
viahoon <hoon@viasofts.com>
2021-06-29 00:58:10 +0900

    처음 로딩때 위성지도로 로딩

--- mygcs/src/main/java/com/viasofts/mygcs/MainActivity.java
+++ mygcs/src/main/java/com/viasofts/mygcs/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.SurfaceTexture;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +17,9 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
 import com.o3dr.android.client.ControlTower;
 import com.o3dr.android.client.Drone;
 import com.o3dr.android.client.interfaces.DroneListener;
@@ -29,18 +33,25 @@ import com.o3dr.services.android.lib.drone.companion.solo.SoloState;
 import com.o3dr.services.android.lib.drone.property.Type;
 import com.o3dr.services.android.lib.gcs.link.LinkConnectionStatus;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-    Handler mainHandler;
     MyDrone mMyDrone;
+    NaverMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainHandler = new Handler(getApplicationContext().getMainLooper());
         mMyDrone = new MyDrone(this);
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -53,5 +64,11 @@ public class MainActivity extends AppCompatActivity {
     public void onStop() {
         super.onStop();
         mMyDrone.stopDrone();
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        mMap = naverMap;
+        naverMap.setMapType(NaverMap.MapType.Satellite);
     }
 }
